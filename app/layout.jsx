1ed3f4d7e7da8c1e1c6ba2f1a53fb8b6afb00cdc
unknown <binyaalex@gmail.com>
2023-06-15 14:27:11 +0300

    create nav

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,8 @@
 import '@styles/globals.css';
 
+import Nav from '@components/Nav'; 
+import Provider from '@components/Provider';
+
 export const metadata = {
     title: "Promptopia",
     description: 'Discover & Share AI Prompts'
@@ -15,6 +18,7 @@ const RootLayout = ({children}) => {
         </body>
 
         <main className='app' >
+            <Nav />
             {children}
         </main>
     </html>
